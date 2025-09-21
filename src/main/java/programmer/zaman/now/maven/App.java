34d121bc82919a51bb0d9f18928fa573d4c2da64
kaiharvest <indradwiprabowo27@gmail.com>
2025-09-21 10:30:31 +0700

    maven dependency

--- src/main/java/programmer/zaman/now/maven/App.java
+++ src/main/java/programmer/zaman/now/maven/App.java
@@ -1,10 +1,19 @@
 package programmer.zaman.now.maven;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Gson gson = new Gson();
+
+        Person person = new Person("Indra Dwi Prabowo");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+
     }
 }
